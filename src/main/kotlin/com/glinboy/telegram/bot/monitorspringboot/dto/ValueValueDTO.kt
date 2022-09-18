5ae44a7a3fb5b4451dcf07ef18a08b762792b973
GLinBoy <hojjatabedie@gmail.com>
2022-09-18 16:54:24 +0200

    📦 NEW: Added ValueValueDTO

---
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/ValueValueDTO.kt
@@ -0,0 +1,9 @@
+package com.glinboy.telegram.bot.monitorspringboot.dto
+
+data class ValueValueDTO(
+    val id: String? = null,
+    val name: String? = null,
+    val description: String? = null,
+    val versionRange: String? = null,
+    val links: ValueLinkDTO? = null
+)
