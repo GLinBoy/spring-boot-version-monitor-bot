2d5e336b87372e88da4f356b51219dd486245b25
GLinBoy <hojjatabedie@gmail.com>
2022-09-18 16:20:45 +0200

    📦 NEW: Initialized by Spring Boot project
    resolved #1

---
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/MonitorSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.glinboy.telegram.bot.monitorspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonitorSpringBootApplication
+
+fun main(args: Array<String>) {
+	runApplication<MonitorSpringBootApplication>(*args)
+}
---
+++ src/test/kotlin/com/glinboy/telegram/bot/monitorspringboot/MonitorSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.glinboy.telegram.bot.monitorspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MonitorSpringBootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
