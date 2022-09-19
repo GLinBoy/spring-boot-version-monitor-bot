e6979bf436b88dc653c2f9b74c352829ebe4dedb
GLinBoy <hojjatabedie@gmail.com>
2022-09-19 19:53:37 +0200

    📦 NEW: Added GradleBuildClassDTO

---
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/GradleBuildClassDTO.kt
@@ -0,0 +1,6 @@
+package com.glinboy.telegram.bot.monitorspringboot.dto
+
+data class GradleBuildClassDTO(
+    val href: String? = null,
+    val templated: Boolean? = null
+)
--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/WelcomeLinkDTO.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/WelcomeLinkDTO.kt
@@ -1,9 +1,9 @@
 package com.glinboy.telegram.bot.monitorspringboot.dto
 
 data class WelcomeLinkDTO(
-    val mavenProject: GradleBuildClass? = null,
-    val mavenBuild: GradleBuildClass? = null,
-    val gradleProject: GradleBuildClass? = null,
-    val gradleBuild: GradleBuildClass? = null,
-    val dependencies: GradleBuildClass? = null
+    val mavenProject: GradleBuildClassDTO? = null,
+    val mavenBuild: GradleBuildClassDTO? = null,
+    val gradleProject: GradleBuildClassDTO? = null,
+    val gradleBuild: GradleBuildClassDTO? = null,
+    val dependencies: GradleBuildClassDTO? = null
 )
