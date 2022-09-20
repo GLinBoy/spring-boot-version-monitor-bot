818651443f3abfca64de502b83f4ed8069db690b
GLinBoy <hojjatabedie@gmail.com>
2022-09-20 17:26:37 +0200

    📦 NEW: Added ReferenceClassDTO

---
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/ReferenceClassDTO.kt
@@ -0,0 +1,7 @@
+package com.glinboy.telegram.bot.monitorspringboot.dto
+
+data class ReferenceClassDTO(
+    val href: String? = null,
+    val title: String? = null,
+    val templated: Boolean? = null
+)
