96d3357941ca396ee005164261cd054fd412913c
GLinBoy <hojjatabedie@gmail.com>
2022-09-20 17:32:48 +0200

    👌 IMPROVE: SpringClient API returned SpringResponseDTO

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/client/SpringClient.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/client/SpringClient.kt
@@ -1,5 +1,7 @@
 package com.glinboy.telegram.bot.monitorspringboot.client
 
+import com.glinboy.telegram.bot.monitorspringboot.dto.SpringResponseDTO
+
 interface SpringClient {
-    fun getSpringMetaData(): String?
+    fun getSpringMetaData(): SpringResponseDTO?
 }
