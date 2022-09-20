6ee1f2bbc2659b595cfe62d616b5deb0860b9310
GLinBoy <hojjatabedie@gmail.com>
2022-09-20 17:32:59 +0200

    👌 IMPROVE: SpringClient Impl. returned SpringResponseDTO

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/client/impl/SpringClientImpl.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/client/impl/SpringClientImpl.kt
@@ -1,6 +1,7 @@
 package com.glinboy.telegram.bot.monitorspringboot.client.impl
 
 import com.glinboy.telegram.bot.monitorspringboot.client.SpringClient
+import com.glinboy.telegram.bot.monitorspringboot.dto.SpringResponseDTO
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Service
 
@@ -9,10 +10,10 @@ class SpringClientImpl: SpringClient {
 
     val restTemplate = RestTemplateBuilder().build()
 
-    override fun getSpringMetaData(): String? {
+    override fun getSpringMetaData(): SpringResponseDTO? {
         return restTemplate.getForObject(
             "https://start.spring.io/metadata/client",
-            String::class.java
+            SpringResponseDTO::class.java
         )
     }
 }
