2b76d474ee7376792317588411f151890417a636
GLinBoy <hojjatabedie@gmail.com>
2022-09-20 18:05:48 +0200

    👌 IMPROVE: Replaced properties with DataDTO

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/SpringResponseDTO.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/SpringResponseDTO.kt
@@ -1,17 +1,19 @@
 package com.glinboy.telegram.bot.monitorspringboot.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class SpringResponseDTO(
-    val links: WelcomeLinkDTO? = null,
-    val dependencies: WelcomeDependencyDTO? = null,
-    val type: TypeDTO? = null,
-    val packaging: BootVersionDTO? = null,
-    val javaVersion: BootVersionDTO? = null,
-    val language: BootVersionDTO? = null,
-    val bootVersion: BootVersionDTO? = null,
-    val groupID: ArtifactIdDTO? = null,
-    val artifactID: ArtifactIdDTO? = null,
-    val version: ArtifactIdDTO? = null,
-    val name: ArtifactIdDTO? = null,
-    val description: ArtifactIdDTO? = null,
-    val packageName: ArtifactIdDTO? = null
+    @JsonProperty("_links") val links: LinksDTO? = null,
+    val dependencies: DependenciesDTO? = null,
+    val type: DataDTO? = null,
+    val packaging: DataDTO? = null,
+    val javaVersion: DataDTO? = null,
+    val language: DataDTO? = null,
+    val bootVersion: DataDTO? = null,
+    val groupID: DataDTO? = null,
+    val artifactID: DataDTO? = null,
+    val version: DataDTO? = null,
+    val name: DataDTO? = null,
+    val description: DataDTO? = null,
+    val packageName: DataDTO? = null
 )
