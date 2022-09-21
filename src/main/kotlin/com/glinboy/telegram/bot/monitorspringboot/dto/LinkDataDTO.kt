fbec67c6e68b518b3227dbe7d1afd97c9174c3a6
GLinBoy <hojjatabedie@gmail.com>
2022-09-21 23:00:56 +0200

    👌 IMPROVE: Renamed to LinkDataDTO

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/LinkDataDTO.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/LinkDataDTO.kt
@@ -1,6 +1,6 @@
 package com.glinboy.telegram.bot.monitorspringboot.dto
 
-data class GradleBuildClassDTO(
+data class LinkDataDTO(
     val href: String? = null,
     val templated: Boolean? = null
 )
