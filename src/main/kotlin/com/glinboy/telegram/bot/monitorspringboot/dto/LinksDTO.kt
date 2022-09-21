439e4bb29a9e8528dcdf38ab91fb53d4372b73ba
GLinBoy <hojjatabedie@gmail.com>
2022-09-21 22:58:34 +0200

    👌 IMPROVE: Renamed to LinksDTO

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/LinksDTO.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/LinksDTO.kt
@@ -1,6 +1,6 @@
 package com.glinboy.telegram.bot.monitorspringboot.dto
 
-data class WelcomeLinkDTO(
+data class LinksDTO(
     val mavenProject: GradleBuildClassDTO? = null,
     val mavenBuild: GradleBuildClassDTO? = null,
     val gradleProject: GradleBuildClassDTO? = null,
