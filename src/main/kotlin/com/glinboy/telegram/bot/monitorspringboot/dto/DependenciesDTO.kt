8ceb3852465fe6e63de3deda255f138f8880eef5
GLinBoy <hojjatabedie@gmail.com>
2022-09-21 23:06:02 +0200

    👌 IMPROVE: Renamed to DependenciesDTO

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/DependenciesDTO.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/DependenciesDTO.kt
@@ -2,5 +2,5 @@ package com.glinboy.telegram.bot.monitorspringboot.dto
 
 data class DependenciesDTO(
     val type: String? = null,
-    val values: List<DependenciesValueDTO>? = null
+    val values: List<DependencyDataDTO>? = null
 )
