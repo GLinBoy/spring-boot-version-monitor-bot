5815be4415f5a625b6d716d6e555afa6045c70a8
GLinBoy <hojjatabedie@gmail.com>
2022-09-21 23:11:02 +0200

    🐛 FIX: Updated JSON property name

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/DependencyValueDTO.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/DependencyValueDTO.kt
@@ -1,9 +1,11 @@
 package com.glinboy.telegram.bot.monitorspringboot.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class DependencyValueDTO(
     val id: String? = null,
     val name: String? = null,
     val description: String? = null,
     val versionRange: String? = null,
-    val links: DependencyValueLinksDTO? = null
+    @JsonProperty("_links") val links: DependencyValueLinksDTO? = null
 )
