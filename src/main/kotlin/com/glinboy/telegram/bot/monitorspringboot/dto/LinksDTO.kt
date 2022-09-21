0d7823d7b792a6a7f7b65c8258face8f6ea76ba9
GLinBoy <hojjatabedie@gmail.com>
2022-09-21 23:01:09 +0200

    👌 IMPROVE: Used LinkDataDTO in LinksDTO

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/LinksDTO.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/LinksDTO.kt
@@ -1,9 +1,9 @@
 package com.glinboy.telegram.bot.monitorspringboot.dto
 
 data class LinksDTO(
-    val mavenProject: GradleBuildClassDTO? = null,
-    val mavenBuild: GradleBuildClassDTO? = null,
-    val gradleProject: GradleBuildClassDTO? = null,
-    val gradleBuild: GradleBuildClassDTO? = null,
-    val dependencies: GradleBuildClassDTO? = null
+    val mavenProject: LinkDataDTO? = null,
+    val mavenBuild: LinkDataDTO? = null,
+    val gradleProject: LinkDataDTO? = null,
+    val gradleBuild: LinkDataDTO? = null,
+    val dependencies: LinkDataDTO? = null
 )
