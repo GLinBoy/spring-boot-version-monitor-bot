aceb2886c876f5d10d14771410509c6c256605be
GLinBoy <hojjatabedie@gmail.com>
2022-09-21 23:05:39 +0200

    👌 IMPROVE: Renamed to DependencyValueLinksDTO

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/DependencyValueLinksDTO.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/DependencyValueLinksDTO.kt
@@ -1,6 +1,6 @@
 package com.glinboy.telegram.bot.monitorspringboot.dto
 
-data class ValueLinkDTO(
+data class DependencyValueLinksDTO(
     val reference: ReferenceUnionDTO? = null,
     val guide: GuideDTO? = null,
     val home: HomeDTO? = null,
