381efbd3af058ed6216e26e8d195e6ab5d5389e9
GLinBoy <hojjatabedie@gmail.com>
2022-09-25 15:23:26 +0200

    👌 IMPROVE: Ignored dependencies and removed null setup of properties

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/SpringResponseDTO.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/dto/SpringResponseDTO.kt
@@ -3,17 +3,21 @@ package com.glinboy.telegram.bot.monitorspringboot.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class SpringResponseDTO(
-    @JsonProperty("_links") val links: LinksDTO? = null,
-    val dependencies: DependenciesDTO? = null,
-    val type: DataDTO? = null,
-    val packaging: DataDTO? = null,
-    val javaVersion: DataDTO? = null,
-    val language: DataDTO? = null,
-    val bootVersion: DataDTO? = null,
-    val groupID: DataDTO? = null,
-    val artifactID: DataDTO? = null,
-    val version: DataDTO? = null,
-    val name: DataDTO? = null,
-    val description: DataDTO? = null,
-    val packageName: DataDTO? = null
+    @JsonProperty("_links") val links: LinksDTO?,
+    /**
+     * We don't need this properties now
+     * and unfortunately there is an strange error under this property
+     */
+//    val dependencies: DependenciesDTO?,
+    val type: DataDTO?,
+    val packaging: DataDTO?,
+    val javaVersion: DataDTO?,
+    val language: DataDTO?,
+    val bootVersion: DataDTO?,
+    val groupID: DataDTO?,
+    val artifactID: DataDTO?,
+    val version: DataDTO?,
+    val name: DataDTO?,
+    val description: DataDTO?,
+    val packageName: DataDTO?
 )
