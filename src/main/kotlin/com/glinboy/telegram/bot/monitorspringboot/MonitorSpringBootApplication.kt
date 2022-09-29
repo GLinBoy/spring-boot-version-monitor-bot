0d17115741b4ffe2d95e3f089230f075c11ad6bf
GLinBoy <hojjatabedie@gmail.com>
2022-09-29 20:31:01 +0200

    👌 IMPROVE: Enabled scheduling

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/MonitorSpringBootApplication.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/MonitorSpringBootApplication.kt
@@ -2,7 +2,9 @@ package com.glinboy.telegram.bot.monitorspringboot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication
 class MonitorSpringBootApplication
 
