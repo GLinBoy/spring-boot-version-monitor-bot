66bbf249e98b04a724c4ff01312683c58b6afea2
GLinBoy <hojjatabedie@gmail.com>
2022-10-01 17:54:18 +0200

    👌 IMPROVE: Added LatestVersionServiceImpl

---
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/impl/LatestVersionServiceImpl.kt
@@ -0,0 +1,28 @@
+package com.glinboy.telegram.bot.monitorspringboot.service.impl
+
+import com.glinboy.telegram.bot.monitorspringboot.dto.event.NewVersionEvent
+import com.glinboy.telegram.bot.monitorspringboot.entity.LatestVersion
+import com.glinboy.telegram.bot.monitorspringboot.repository.LatestVersionRepository
+import com.glinboy.telegram.bot.monitorspringboot.service.LatestVersionService
+import org.slf4j.LoggerFactory
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class LatestVersionServiceImpl(
+    val repository: LatestVersionRepository
+) : LatestVersionService {
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    override fun isLatestVersion(v: String): Boolean = !repository.existsById(v)
+
+    @EventListener(NewVersionEvent::class)
+    @Transactional
+    override fun updateVersion(v: NewVersionEvent) {
+        repository.deleteAllByIdStartsWith(v.version.substring(0, v.version.lastIndexOf('.')))
+        repository.save(LatestVersion(v.version))
+    }
+}
