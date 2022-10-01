647c5b3e99a0284e809b7a042cc8606cdcc89daa
GLinBoy <hojjatabedie@gmail.com>
2022-10-01 18:38:38 +0200

    👌 IMPROVE: Read version url from application properties

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/client/impl/SpringClientImpl.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/client/impl/SpringClientImpl.kt
@@ -2,17 +2,21 @@ package com.glinboy.telegram.bot.monitorspringboot.client.impl
 
 import com.glinboy.telegram.bot.monitorspringboot.client.SpringClient
 import com.glinboy.telegram.bot.monitorspringboot.dto.SpringResponseDTO
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
 
 @Service
 class SpringClientImpl: SpringClient {
 
-    val restTemplate = RestTemplateBuilder().build()
+    val restTemplate: RestTemplate = RestTemplateBuilder().build()
 
+    @Value("\${application.versions-url.spring-boot}")
+    lateinit var springBootUrl: String
     override fun getSpringMetaData(): SpringResponseDTO? {
         return restTemplate.getForObject(
-            "https://start.spring.io/metadata/client",
+            springBootUrl,
             SpringResponseDTO::class.java
         )
     }
