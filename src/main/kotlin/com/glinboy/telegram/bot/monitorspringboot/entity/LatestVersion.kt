8bafc6a8482d9b42e08e845abd220d6fc4400bf4
GLinBoy <hojjatabedie@gmail.com>
2022-10-01 17:46:21 +0200

    👌 IMPROVE: Added LatestVersion entity

---
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/entity/LatestVersion.kt
@@ -0,0 +1,9 @@
+package com.glinboy.telegram.bot.monitorspringboot.entity
+
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+data class LatestVersion(
+    @Id val version: String?
+)
