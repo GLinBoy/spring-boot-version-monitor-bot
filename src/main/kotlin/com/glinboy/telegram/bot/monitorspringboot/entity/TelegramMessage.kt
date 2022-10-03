811b5433c9d3d1871a8a736c493443d3130f58e2
GLinBoy <hojjatabedie@gmail.com>
2022-10-03 15:48:05 +0200

    👌 IMPROVE: Added TelegramMessage entity

---
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/entity/TelegramMessage.kt
@@ -0,0 +1,22 @@
+package com.glinboy.telegram.bot.monitorspringboot.entity
+
+import javax.persistence.*
+
+@Entity
+data class TelegramMessage(
+    @Id val messageId: Long? = null,
+    @ManyToOne(cascade = [CascadeType.ALL])
+    @JoinColumn(
+        name = "user_id", nullable = false,
+        foreignKey = ForeignKey(name = "fk_user_message")
+    )
+    val from: TelegramUser? = null,
+    @ManyToOne(cascade = [CascadeType.ALL])
+    @JoinColumn(
+        name = "chat_id", nullable = false,
+        foreignKey = ForeignKey(name = "fk_chat_message")
+    )
+    val chat: TelegramChat? = null,
+    val date: Long? = null,
+    val text: String? = null
+)
