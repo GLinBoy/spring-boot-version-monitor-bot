b3036c14cceeae8ab1d487609155ea4fad48e228
GLinBoy <hojjatabedie@gmail.com>
2022-10-03 15:51:44 +0200

    👌 IMPROVE: Added a service impl. for TelegramUser entity

---
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/impl/TelegramUserServiceImpl.kt
@@ -0,0 +1,8 @@
+package com.glinboy.telegram.bot.monitorspringboot.service.impl
+
+import com.glinboy.telegram.bot.monitorspringboot.service.TelegramUserService
+import org.springframework.stereotype.Service
+
+@Service
+class TelegramUserServiceImpl : TelegramUserService {
+}
