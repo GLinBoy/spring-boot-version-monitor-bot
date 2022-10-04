a9e1f0fadff6c292020ef7244dde620286368660
GLinBoy <hojjatabedie@gmail.com>
2022-10-04 23:06:06 +0200

    👌 IMPROVE: Extended TelegramChatServiceImpl from GenericServiceImpl

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/impl/TelegramChatServiceImpl.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/impl/TelegramChatServiceImpl.kt
@@ -1,8 +1,14 @@
 package com.glinboy.telegram.bot.monitorspringboot.service.impl
 
+import com.glinboy.telegram.bot.monitorspringboot.entity.TelegramChat
+import com.glinboy.telegram.bot.monitorspringboot.repository.TelegramChatRepository
 import com.glinboy.telegram.bot.monitorspringboot.service.TelegramChatService
 import org.springframework.stereotype.Service
 
 @Service
-class TelegramChatServiceImpl : TelegramChatService {
+class TelegramChatServiceImpl(
+    private val repository: TelegramChatRepository
+) : TelegramChatService,
+    GenericServiceImpl<TelegramChat, Long, TelegramChatRepository>(repository){
+    override fun findAllChatId(): List<Long> = repository.findAllIdOnly()
 }
