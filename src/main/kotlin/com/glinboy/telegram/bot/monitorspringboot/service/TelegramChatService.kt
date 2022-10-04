f1e9786bf96499b5c1e4e41e8010eac5216d73ba
GLinBoy <hojjatabedie@gmail.com>
2022-10-04 23:04:47 +0200

    👌 IMPROVE: Extended TelegramChatService from GenericService

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/TelegramChatService.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/TelegramChatService.kt
@@ -1,4 +1,7 @@
 package com.glinboy.telegram.bot.monitorspringboot.service
 
-interface TelegramChatService {
+import com.glinboy.telegram.bot.monitorspringboot.entity.TelegramChat
+
+interface TelegramChatService : GenericService<TelegramChat, Long> {
+    fun findAllChatId(): List<Long>
 }
