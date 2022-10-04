72187adcc78d859d7cbb82a676b9d22ba79f2f5d
GLinBoy <hojjatabedie@gmail.com>
2022-10-04 23:05:44 +0200

    👌 IMPROVE: Extended TelegramUserServiceImpl from GenericServiceImpl

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/impl/TelegramUserServiceImpl.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/impl/TelegramUserServiceImpl.kt
@@ -1,8 +1,13 @@
 package com.glinboy.telegram.bot.monitorspringboot.service.impl
 
+import com.glinboy.telegram.bot.monitorspringboot.entity.TelegramUser
+import com.glinboy.telegram.bot.monitorspringboot.repository.TelegramUserRepository
 import com.glinboy.telegram.bot.monitorspringboot.service.TelegramUserService
 import org.springframework.stereotype.Service
 
 @Service
-class TelegramUserServiceImpl : TelegramUserService {
+class TelegramUserServiceImpl(
+    private val repository: TelegramUserRepository
+) : TelegramUserService,
+    GenericServiceImpl<TelegramUser, Long, TelegramUserRepository>(repository) {
 }
