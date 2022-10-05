5bebd05cdb272344cb5a32b9ae15c0b9fe707b6f
GLinBoy <hojjatabedie@gmail.com>
2022-10-05 22:23:10 +0200

    👌 IMPROVE: Added a method to get all chat id to the TelegramChatRepository

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/repository/TelegramChatRepository.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/repository/TelegramChatRepository.kt
@@ -2,6 +2,9 @@ package com.glinboy.telegram.bot.monitorspringboot.repository
 
 import com.glinboy.telegram.bot.monitorspringboot.entity.TelegramChat
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface TelegramChatRepository : JpaRepository<TelegramChat, Long> {
+    @Query("select tc.id from TelegramChat tc")
+    fun findAllIdOnly(): List<Long>
 }
