31f00b656141570de47b25f5895f8c2b5d865c7d
GLinBoy <hojjatabedie@gmail.com>
2022-10-06 20:09:03 +0200

    👌 IMPROVE: Extracted telegram bot dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ repositories {
 	mavenCentral()
 }
 
+val telegrambotsVersion = "6.1.0"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
@@ -23,7 +25,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.telegram:telegrambots:6.1.0")
+    implementation("org.telegram:telegrambots:$telegrambotsVersion")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
