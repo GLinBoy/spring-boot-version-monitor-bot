f51eaf2bf2b1e1734e04a884ecd7544364e122a9
GLinBoy <hojjatabedie@gmail.com>
2022-10-06 20:47:01 +0200

    👌 IMPROVE: Remove unnecessary method
    #32

--- src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/TelegramService.kt
+++ src/main/kotlin/com/glinboy/telegram/bot/monitorspringboot/service/TelegramService.kt
@@ -1,12 +1,6 @@
 package com.glinboy.telegram.bot.monitorspringboot.service
 
-import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.context.event.EventListener
-
 interface TelegramService {
-
-    @EventListener(ApplicationReadyEvent::class)
-    fun registerBot()
 
     fun publishNewVersion(version: String)
 }
