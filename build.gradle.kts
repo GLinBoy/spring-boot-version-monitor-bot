b0fc3ca9faaee9af97cf50136712062ede847678
GLinBoy <hojjatabedie@gmail.com>
2022-12-28 15:38:45 +0100

    👌 IMPROVE: Upgraded ben-manes.versions [0.43.0 -> 0.44.0]
    #55

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 	kotlin("jvm") version "1.7.21"
 	kotlin("plugin.spring") version "1.7.21"
     id("org.ec4j.editorconfig") version "0.0.3"
-    id("com.github.ben-manes.versions") version "0.43.0"
+    id("com.github.ben-manes.versions") version "0.44.0"
 }
 
 group = "com.glinboy.telegram.bot"
