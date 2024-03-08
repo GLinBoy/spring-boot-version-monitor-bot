95a9f4316a4c73a17930b6e1a793afe993e8f0e8
GLinBoy <glinboy@live.com>
2024-03-08 18:44:06 +0100

    👌 IMPROVE: Upgraded dependency ben-manes.versions" plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
     id("org.ec4j.editorconfig") version "0.1.0"
-    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "com.glinboy.telegram.bot"
